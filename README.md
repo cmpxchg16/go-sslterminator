a2ea501b5d6fe26b1beed34b2add3e4efe707cbd
Uri Shamay <cmpxchg16@gmail.com>
2016-03-08 21:27:31 +0200

    small fixes & more readme

--- README.md
+++ README.md
@@ -4,26 +4,41 @@ Introduction
 Very simple (Thanks to Go!) SSL terminator proxy.
 Use it when you need to terminate SSL sessions transparently as a MITM interceptor to read the plain raw data.
 
-
 Usage
 ================
 
-1. run some http server on port 80
-2. go run go-sslterminator.go
-3. curl -k https://localhost:443/
+1. run some http server on port 8000
+2. go run go-sslterminator.go // assumed you have key.pem & cert.pem in `pwd`
+3. curl -v -k https://localhost:44300/
 
+If you want to do some quick test, you can use that commands:
+
+1. create SSL key & certificate for go-sslterminator
+	openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -nodes -subj "/C=GO/ST=Golang/L=Golang/O=Golang/OU=IT Department/CN=golang.org"
+2. run dummy http server
+	while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; } | nc -l 8000; done
+3. run go-sslterminator
+	go run go-sslterminator.go
+3. run dummy ssl client
+	curl -v -k https://localhost:44300/
 
 Help
 ================
 
-go run go-sslterminator.go --help
-
+	go run go-sslterminator.go --help
+	  -b string
+    		backend address (default ":8000")
+  	  -c string
+    		SSL certificate path (default "cert.pem")
+  	  -k string
+    		SSL key path (default "key.pem")
+          -l string
+    		local address (default ":44300")
 
 License
 ================
 
 Licensed under the New BSD License.
-
 
 Author
 ================
--- go-sslterminator.go
+++ go-sslterminator.go
@@ -2,52 +2,51 @@ package main
 
 import (
 	"crypto/tls"
+	"flag"
+	"io"
 	"log"
 	"net"
-	"io"
 	"runtime"
-	"flag"
 )
-    
+
 var localAddress string
 var backendAddress string
 var certificatePath string
 var keyPath string
 
 func init() {
-	flag.StringVar(&localAddress, "l", ":443", "local address")
-	flag.StringVar(&backendAddress, "b", ":80", "backend address")
-	flag.StringVar(&certificatePath, "c", "server.pem", "SSL certificate path")
-	flag.StringVar(&keyPath, "k", "server.key", "SSL key path")
+	flag.StringVar(&localAddress, "l", ":44300", "local address")
+	flag.StringVar(&backendAddress, "b", ":8000", "backend address")
+	flag.StringVar(&certificatePath, "c", "cert.pem", "SSL certificate path")
+	flag.StringVar(&keyPath, "k", "key.pem", "SSL key path")
 }
-    
+
 func main() {
 	flag.Parse()
-	
+
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	
+
 	cert, err := tls.LoadX509KeyPair(certificatePath, keyPath)
 	if err != nil {
 		log.Fatalf("error in tls.LoadX509KeyPair: %s", err)
 	}
-	
-	config := tls.Config{Certificates: []tls.Certificate{cert}, 
-						 CipherSuites: []uint16{tls.TLS_RSA_WITH_AES_256_CBC_SHA}}
-	
+
+	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+
 	listener, err := tls.Listen("tcp", localAddress, &config)
 	if err != nil {
 		log.Fatalf("error in tls.Listen: %s", err)
 	}
-	
+
 	log.Printf("local server on: %s, backend server on: %s", localAddress, backendAddress)
-	
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Printf("error in listener.Accept: %s", err)
 			break
 		}
-		
+
 		go handle(conn)
 	}
 }
@@ -55,24 +54,35 @@ func main() {
 func handle(clientConn net.Conn) {
 	tlsconn, ok := clientConn.(*tls.Conn)
 	if ok {
-		
+
 		err := tlsconn.Handshake()
 		if err != nil {
 			log.Printf("error in tls.Handshake: %s", err)
+			clientConn.Close()
+			return
 		}
-		
+
 		backendConn, err := net.Dial("tcp", backendAddress)
 		if err != nil {
 			log.Printf("error in net.Dial: %s", err)
+			clientConn.Close()
+			return
 		}
 
 		go Tunnel(clientConn, backendConn)
-        	go Tunnel(backendConn, clientConn)
+		go Tunnel(backendConn, clientConn)
 	}
 }
 
 func Tunnel(from, to io.ReadWriteCloser) {
-    io.Copy(from, to)
-    from.Close()
-    to.Close()
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("recovered while tunneling")
+		}
+	}()
+
+	io.Copy(from, to)
+	to.Close()
+	from.Close()
+	log.Printf("tunneling is done")
 }
